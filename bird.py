2c2c712018e7f95d2b5625acc92e7a833955b073
csehidanieltamas <csehi.daniel.tamas@students.jedlik.eu>
2026-03-11 18:45:23 +0100

    kis javítások

--- bird.py
+++ bird.py
@@ -8,13 +8,14 @@ class Bird:
         self.y: float = y
         self.y_vel: float = 0
         self.player_img = pygame.image.load("imageSource/Flappy Bird Chicken.png").convert_alpha()
+        self.rect = self.player_img.get_rect(topleft=(x,y))
 
     def draw(self, win: pygame.Surface):
-        win.blit(self.player_img, (self.x, self.y))
+        win.blit(self.player_img, self.rect)
 
     def move(self):
         self.y_vel -= 10
-        self.y += self.y_vel
+        self.rect.y += int(self.y_vel)
 
 
     def jump(self):
