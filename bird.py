96e697676ac9795f1c6087aa6fbd72aae0fe0fcf
boksaylevente2009 <boksay.levente@students.jedlik.eu>
2026-03-11 07:36:24 +0100

    linter formázás

--- bird.py
+++ bird.py
@@ -1,6 +1,7 @@
 import pygame
 from settings import COLOR, MAX_VEL
 
+
 class Bird:
     def __init__(self, x: int, y: int, radius: int):
         self.x: float = x
@@ -11,8 +12,5 @@ class Bird:
     def draw(self, win: pygame.Surface):
         pygame.draw.circle(win, COLOR, (self.x, self.y), self.radius)
 
-
     def move(self):
         self.y += self.y_vel
-
-    
