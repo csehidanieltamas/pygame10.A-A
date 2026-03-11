7e78f868364263555112cfa7a10a3c429c6057d3
boksaylevente2009 <boksay.levente@students.jedlik.eu>
2026-03-11 08:32:53 +0100

    pipe és image mappa

---
+++ pipe.py
@@ -0,0 +1,6 @@
+import pygame
+
+class Pipe:
+    pipe_surface: pygame.image.load() = pygame.image.load('imageSource/pipe.jpg')
+    pipe_surface: pygame.transform.scale2x() = pygame.transform.scale2x(pipe_surface)
+    
