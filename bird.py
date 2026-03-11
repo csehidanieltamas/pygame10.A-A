78f04547ee3d1180655ebffedd3ea3f11745e95b
figdermark <figder.mark@students.jedlik.eu>
2026-03-11 08:44:03 +0100

    madár hozzáadása és változtatás a bird.pyban

--- bird.py
+++ bird.py
@@ -1,16 +1,16 @@
 import pygame
-from settings import COLOR, MAX_VEL
+from settings import VEL
 
 
 class Bird:
-    def __init__(self, x: int, y: int, radius: int):
+    def __init__(self, x: int, y: int):
         self.x: float = x
         self.y: float = y
-        self.radius = radius
-        self.y_vel: float = MAX_VEL
+        self.y_vel: float = VEL
+        self.player_img = pygame.image.load("imageSource/Flappy Bird Chicken.png").convert_alpha()
 
     def draw(self, win: pygame.Surface):
-        pygame.draw.circle(win, COLOR, (self.x, self.y), self.radius)
+        win.blit(self.player_img, (self.x, self.y))
 
     def move(self):
         self.y += self.y_vel
