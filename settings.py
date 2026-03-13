fd9e6f7c114dd20c63a47ee83178ecc6c7d14506
boksaylevente2009 <boksay.levente@students.jedlik.eu>
2026-03-13 13:25:03 +0100

    ugrás és void

--- settings.py
+++ settings.py
@@ -7,7 +7,7 @@ WHITE: tuple[int, int, int] = (255, 255, 255)
 BLACK: tuple[int, int, int] = (0, 0, 0)
 COLOR: tuple[int, int, int] = WHITE
 
-VEL: int = 4
+VEL: int = -12
 
 PIPE_VEL: int = 5
 PIPE_GAP: int = 200
