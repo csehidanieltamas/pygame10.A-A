796bca8c5f50644c473a81380b14645d0e27d7b5
boksaylevente2009 <boksay.levente@students.jedlik.eu>
2026-03-13 09:06:18 +0100

    formázás

--- pipe.py
+++ pipe.py
@@ -3,18 +3,21 @@ import pygame
 from bird import Bird
 from settings import HEIGHT, PIPE_VEL, PIPE_GAP
 
+
 class Pipe:
-    PIPE_IMAGE: pygame.Surface = pygame.transform.scale2x(pygame.image.load("imageSource/pipe.jpg"))
+    PIPE_IMAGE: pygame.Surface = pygame.transform.scale2x(
+        pygame.image.load("imageSource/pipe.jpg")
+    )
     FLIPPED_PIPE: pygame.Surface = pygame.transform.flip(PIPE_IMAGE, False, True)
 
     def __init__(self, x: int) -> None:
         self.x: int = x
         self.passed: bool = False
-        
-        self.height: int = random.randint(100, HEIGHT - 300) 
+
+        self.height: int = random.randint(100, HEIGHT - 300)
 
         self.top_y: int = self.height - self.PIPE_IMAGE.get_height()
-        
+
         self.bottom_y: int = self.height + PIPE_GAP
 
     def move(self) -> None:
@@ -26,15 +29,25 @@ class Pipe:
 
     def collide(self, bird: Bird) -> bool:
         bird_rect: pygame.Rect = pygame.Rect(
-            int(bird.x - bird.radius), 
-            int(bird.y - bird.radius), 
-            bird.radius * 2, 
-            bird.radius * 2
+            int(bird.x - bird.radius),
+            int(bird.y - bird.radius),
+            bird.radius * 2,
+            bird.radius * 2,
         )
-        
-        top_rect: pygame.Rect = pygame.Rect(self.x, self.top_y, self.FLIPPED_PIPE.get_width(), self.FLIPPED_PIPE.get_height())
-        bottom_rect: pygame.Rect = pygame.Rect(self.x, self.bottom_y, self.PIPE_IMAGE.get_width(), self.PIPE_IMAGE.get_height())
-        
+
+        top_rect: pygame.Rect = pygame.Rect(
+            self.x,
+            self.top_y,
+            self.FLIPPED_PIPE.get_width(),
+            self.FLIPPED_PIPE.get_height(),
+        )
+        bottom_rect: pygame.Rect = pygame.Rect(
+            self.x,
+            self.bottom_y,
+            self.PIPE_IMAGE.get_width(),
+            self.PIPE_IMAGE.get_height(),
+        )
+
         if bird_rect.colliderect(top_rect) or bird_rect.colliderect(bottom_rect):
             return True
         return False
