ed2934f9ce2a68fc82cc08635a4e16106bcd2a82
csehidanieltamas <csehi.daniel.tamas@students.jedlik.eu>
2026-03-13 19:30:30 +0100

    nehézség 1

--- pipe.py
+++ pipe.py
@@ -1,7 +1,6 @@
 import random
 import pygame
-from settings import PIPE_VEL, PIPE_GAP
-
+import settings
 
 class Pipe:
     def __init__(self, x: int) -> None:
@@ -9,11 +8,11 @@ class Pipe:
         self.passed: bool = False
         self.player_img = pygame.image.load("imageSource/Brick Pipe.png").convert_alpha()
         self.rect = self.player_img.get_rect(topleft=(x,self.y))
-        self.toprect = self.player_img.get_rect(topleft=(x, self.y - PIPE_GAP))
+        self.toprect = self.player_img.get_rect(topleft=(x, self.y - settings.PIPE_GAP))
 
     def move(self) -> None:
-        self.rect.x -= PIPE_VEL
-        self.toprect.x -= PIPE_VEL
+        self.rect.x -= settings.PIPE_VEL
+        self.toprect.x -= settings.PIPE_VEL
 
     def draw(self, win: pygame.Surface) -> None:
         win.blit(pygame.transform.flip(self.player_img, False, True), self.toprect)
