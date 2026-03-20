be3a54674f267dc2cfd4f18c191b229de4e76407
csehidanieltamas <csehi.daniel.tamas@students.jedlik.eu>
2026-03-20 13:57:59 +0100

    kommentelések hozzáadása

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from game import Game
 
 def main() -> None:
-    game: Game = Game()
-    game.difficulty()
+    game: Game = Game() # objetkum a játékmenettel létrehozása
+    game.difficulty() # játékmenet elindul a nehézség választással
 
 if __name__ == "__main__":
     main()
