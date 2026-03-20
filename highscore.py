363c1e03aefe3130d8958f1000bbdeac0509cead
boksaylevente2009 <boksay.levente@students.jedlik.eu>
2026-03-20 14:04:56 +0100

    highscore

---
+++ highscore.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+
+class Highscore:
+    __aktuális_dátum: str = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    __score: int
+    __gamemode: str | None
+
+    def __init__(self, score: int, gamemode: str | None) -> None:
+        self.__score = score
+        self.__gamemode = gamemode
+
+    def highscore_kiírás(self) -> None:
+        with open("Highscore.txt", "w", encoding="utf-8") as file:
+            file.write(
+                f"Pont: {self.__score}, Nehézség: {self.__gamemode}, Dátum: {self.__aktuális_dátum}"
+            )
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from game import Game
 
+
 def main() -> None:
     game: Game = Game()
     game.difficulty()
+
 
 if __name__ == "__main__":
     main()
