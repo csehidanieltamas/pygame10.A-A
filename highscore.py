5852ab9c5505ed8a3060f8c594d4d15e51dfc77f
boksaylevente2009 <boksay.levente@students.jedlik.eu>
2026-03-20 20:17:04 +0100

    gamemode típus hiba kijav.

--- highscore.py
+++ highscore.py
@@ -4,9 +4,9 @@ from datetime import datetime
 class Highscore:
     __aktuális_dátum: str = datetime.now().strftime("%Y.%m.%d %H:%M:%S")
     __score: int
-    __gamemode: str | None
+    __gamemode: str
 
-    def __init__(self, score: int, gamemode: str | None) -> None:
+    def __init__(self, score: int, gamemode: str) -> None:
         self.__score = score
         self.__gamemode = gamemode
 
