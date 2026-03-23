6bcfc634159b44b23ece2a21586a30c0bf9b4ec7
boksaylevente2009 <boksay.levente@students.jedlik.eu>
2026-03-23 11:00:46 +0100

    formázás

--- bg.py
+++ bg.py
@@ -4,17 +4,17 @@ from settings import WIDTH
 
 class Bg:
     def __init__(self) -> None:
-        self._x: int = 0 # háttér vízszintes pozíciója
+        self._x: int = 0  # háttér vízszintes pozíciója
         self._player_img: pygame.Surface = pygame.image.load(
             "imageSource/Background.png"
-        ) # háttér kép betöltése
+        )  # háttér kép betöltése
 
-    def move(self) -> None: # háttér mozgatása lassan balra
+    def move(self) -> None:  # háttér mozgatása lassan balra
         self._x -= 1
         if self._x <= -WIDTH:
             self._x = 0
 
-    def draw(self, win: pygame.Surface) -> None: # háttér rajzolása
+    def draw(self, win: pygame.Surface) -> None:  # háttér rajzolása
         win.blit(self._player_img, (self._x, 0))
         win.blit(self._player_img, (self._x + WIDTH, 0))
         # két háttér, az egyik egy képernyővel balra a tökéletes kapcsolódásért
--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from game import Game
 
 
 def main() -> None:
-    game: Game = Game() # objetkum a játékmenettel létrehozása
-    game.difficulty() # játékmenet elindul a nehézség választással
+    game: Game = Game()  # objetkum a játékmenettel létrehozása
+    game.difficulty()  # játékmenet elindul a nehézség választással
 
 
 if __name__ == "__main__":
